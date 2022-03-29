0583a66849a66a9ad813fe667797207c3b3129d7
成都_陈杰 <397700756@qq.com>
2022-03-29 20:28:59 +0800

    spring bean初始化

---
+++ spring-debug/src/main/java/org/springframework/debug/MyFactoryBean.java
@@ -0,0 +1,24 @@
+package org.springframework.debug;
+
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.debug.supplier.User;
+
+public class MyFactoryBean implements FactoryBean<User> {
+
+    @Override
+    public User getObject() throws Exception {
+        //任何创建对象的操作
+        return new User("zhangsan");
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return User.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+}
--- spring-debug/src/main/java/org/springframework/debug/Test.java
+++ spring-debug/src/main/java/org/springframework/debug/Test.java
@@ -1,6 +1,6 @@
 package org.springframework.debug;
 
-import org.springframework.debug.selfeditor.Customer;
+import org.springframework.debug.supplier.User;
 
 /**
  *<P>@author: 陈杰
@@ -14,9 +14,13 @@ public class Test {
 
 		MyClassPathXMLApplicationContext ac = new MyClassPathXMLApplicationContext("applicationContext.xml");
 
+		MyFactoryBean bean1 = (MyFactoryBean) ac.getBean( "&myFactoryBean");
+        System.out.println(bean1);
+        User bean = (User) ac.getBean("myFactoryBean");
+        System.out.println(bean.getUsername());
 //		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("test.xml");
 //        Person person = ac.getBean("person", Person.class);
-//        ac.close();
+//        ac.close();postProcessBeanFactory
 //        Person person2 = ac.getBean("person", Person.class);
 //		MyClassPathXMLApplicationContext ac = new MyClassPathXMLApplicationContext("selfEditor.xml");
 //		Customer bean = ac.getBean(Customer.class);
