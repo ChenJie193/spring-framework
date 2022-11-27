040b8ccaaa8145ed959d1babec02a2b54afc8863
成都_陈杰 <397700756@qq.com>
2022-11-27 21:35:11 +0800

    自定义xml标签

---
+++ spring-debug/src/main/java/org/springframework/debug/selftag/UserBeanDefinitionParser.java
@@ -0,0 +1,31 @@
+package org.springframework.debug.selftag;
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.util.StringUtils;
+import org.w3c.dom.Element;
+
+public class UserBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
+
+	@Override
+	protected Class<?> getBeanClass(Element element) {
+		return User.class;
+	}
+
+	@Override
+	protected void doParse(Element element, BeanDefinitionBuilder builder) {
+		String userName = element.getAttribute("username");
+		String email = element.getAttribute("email");
+		String password = element.getAttribute("password");
+
+		if(StringUtils.hasText(userName)){
+			builder.addPropertyValue("userName",userName);
+		}
+		if(StringUtils.hasText(email)){
+			builder.addPropertyValue("email",email);
+		}
+		if(StringUtils.hasText(password)){
+			builder.addPropertyValue("password",password);
+		}
+	}
+}
---
+++ spring-debug/src/main/java/org/springframework/debug/selftag/UserNamespaceHandler.java
@@ -0,0 +1,10 @@
+package org.springframework.debug.selftag;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class UserNamespaceHandler extends NamespaceHandlerSupport {
+	@Override
+	public void init() {
+		registerBeanDefinitionParser("user",new UserBeanDefinitionParser());
+	}
+}
